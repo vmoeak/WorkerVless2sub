61276c94318c99d911465c74969a66ea6196b2ec
vmoeak <30279416+vmoeak@users.noreply.github.com>
2024-06-03 00:34:20 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -29,7 +29,8 @@ let addressesnotlsapi = [
 
 let DLS = 8;//速度下限
 let addressescsv = [
-	//'https://raw.githubusercontent.com/cmliu/WorkerVless2sub/main/addressescsv.csv', //iptest测速结果文件。
+	'https://raw.githubusercontent.com/cmliu/WorkerVless2sub/main/addressescsv.csv', //iptest测速结果文件。
+	'https://raw.githubusercontent.com/vmoeak/ip/main/45102-1717344633.csv?token=GHSAT0AAAAAACSGK2JPEFUM33HEHZDVVPF4ZS4TZOQ'
 ];
 
 let subconverter = "url.v1.mk"; //在线订阅转换后端，目前使用肥羊的订阅转换功能。支持自建psub 可自行搭建https://github.com/bulianglin/psub
